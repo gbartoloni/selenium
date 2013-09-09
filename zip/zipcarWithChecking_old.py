b859ba22d967f80455ca9f95db81b45475699c26
guido bartoloni <gbartoloni@hotmail.com>
2013-09-08 23:40:58 -0400

    added more elements to DB and cleaned up code

--- zip/zipcarWithChecking_old.py
+++ zip/zipcarWithChecking_old.py
@@ -16,10 +16,11 @@ class Menues1(unittest.TestCase):
     def test_menues1(self):
         driver = self.driver
         driver.get(self.base_url )
+        #try: self.assertEqual("Car Sharing, an alternative to car rental and car ownership - Zipcar", driver.title)
 	try: re.match("Car Sharing.*",driver.title)
         except AssertionError as e: self.verificationErrors.append(str(e))
 	#Here we click on the "how it works" tab then retrieve from the DB the ids to be matched"
-        self.matchElements("is zipcar for me?")
+        self.matchElements("how it works")
     
     def matchElements(self,w):
         con = lite.connect('zip.db')
@@ -28,21 +29,24 @@ class Menues1(unittest.TestCase):
 	cmpDriver=self.driver
         cmpDriver.find_element_by_link_text(w).click()
 	print "matching elements for:" + w
+	#allPage=cmpDriver.find_elements_by_xpath("//*")
         with con:    
             cur = con.cursor()    
             cur.execute("SELECT element FROM elements where window=:win", {"win" : w})
             rows = cur.fetchall()
             for element in rows:
-		print "found element in DB:" + element[0] + " now finding it in page '" + w + "'"
+		print "found element in DB:" + element[0]
 	        try: cmpDriver.find_element_by_id(element[0])
         	except NoSuchElementException, e: return False
-        	except: raise 
+		#Future implementation of search by text and by element name		
+		#elem = cmpDriver.find_element_by_xpath("//body[contains(text()),element[0]]")
+		#self.is_element_present("name",element[0])
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException, e: return False
         return True
-
+    
     def is_alert_present(self):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException, e: return False
